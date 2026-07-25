e373454f67ed2dbdec4801745cec38a7e63545f1
patilsuman749-oss <patilsuman749@gmail.com>
2026-07-25 22:15:52 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,36 @@
 
+import {
+    auth,
+    db,
+    onAuthStateChanged,
+    doc,
+    getDoc
+} from "./firebase.js";
+
+onAuthStateChanged(auth, async (user) => {
+
+    if (!user) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (!userSnap.exists()) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const userData = userSnap.data();
+
+    if (!userData.isAdmin) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("adminPanel").style.display = "block";
+
+    alert("Welcome Admin 🍿");
+});
