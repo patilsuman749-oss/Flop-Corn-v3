73970ce99f0dd68968d73a3cc699b3fd7240efeb
patilsuman749-oss <patilsuman749@gmail.com>
2026-08-01 20:52:16 +0530

    Update compare.js

--- compare.js
+++ compare.js
@@ -789,19 +789,3 @@ function celebrateWinner(){
     }
 
 }
-/* ==========================================
-   MOBILE MENU
-========================================== */
-
-const mobileMenuButton = document.getElementById("mobileMenuButton");
-const mobileMenu = document.getElementById("mobileMenu");
-
-if (mobileMenuButton && mobileMenu) {
-
-    mobileMenuButton.addEventListener("click", () => {
-
-        mobileMenu.classList.toggle("active");
-
-    });
-
-}
